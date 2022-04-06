7bd9f03f43d5b975ce9a1e55ba2838099b0e1a3c
ILYLS <damien.gauthier@live.com>
2022-04-06 11:46:56 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatGridListModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
